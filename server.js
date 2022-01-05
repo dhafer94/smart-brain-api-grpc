8cd56f7791d8b52088fd54471b90b738a55e674e
Dhafer Alzubairi <dx.dhafer@gmail.com>
2022-01-05 02:39:50 +0200

    added port

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db); });
 app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res); });
 
 
-app.listen(3001, () => {
-    console.log('app is running on port 3001');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 });
 
